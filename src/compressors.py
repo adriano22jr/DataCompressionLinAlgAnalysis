0aab43e24455829a88fbd7a8f107a63bdbbb88be
adriano22_ <califano993@gmail.com>
2024-11-27 17:51:40 +0100

    Added more compression algorithm
    Needs testing

--- src/compressors.py
+++ src/compressors.py
@@ -14,22 +14,10 @@ def compressor_HEVCVS(input_path, output_path):
     
     # call ffmpeg to compress the video
     subprocess.run(["../../bin/ffmpeg", "-framerate", "120", "-i", input_file, "-crf", "18", "-c:v", "libx265", output_file])
-    
-    # compress ratio evaluation
-    size = 0
-    folderpath = os.path.abspath(os.path.dirname(input_path))
-    for path, dirs, files in os.walk(folderpath):
-        for f in files:
-            fp = os.path.join(path, f)
-            size += os.path.getsize(fp)
-            
+
     endTime = time.time()
-    
-    videosize = os.stat(os.path.abspath(output_path)).st_size   
-    print(f"encoded in {endTime - startTime} seconds.")  # print the time taken to encode the video
-    print("compression ratio: " + str(size/videosize))      
-            
-            
+    print(f"Encoded in {endTime - startTime} seconds.")  # print the time taken to encode the video
+                 
 
 def compressor_AV1VS(input_path, output_path):
     # set input and output paths
@@ -41,19 +29,8 @@ def compressor_AV1VS(input_path, output_path):
     # call ffmpeg to compress the video
     subprocess.run(["../../bin/ffmpeg", "-framerate", "120", "-i", input_file, "-crf", "3", "-c:v", "libaom-av1", output_file])
     
-    # compress ratio evaluation
-    size = 0
-    folderpath = os.path.abspath(os.path.dirname(input_path))
-    for path, dirs, files in os.walk(folderpath):
-        for f in files:
-            fp = os.path.join(path, f)
-            size += os.path.getsize(fp)
-            
     endTime = time.time()
-
-    videosize = os.stat(os.path.abspath(output_path)).st_size  
-    print(f"encoded in {endTime - startTime} seconds.")  # print the time taken to encode the video
-    print("compression ratio: " + str(size/videosize))    
+    print(f"Encoded in {endTime - startTime} seconds.")  # print the time taken to encode the video
 
 
 def compressor_VP9VS(input_path, output_path):
@@ -65,17 +42,90 @@ def compressor_VP9VS(input_path, output_path):
     
     # call ffmpeg to compress the video
     subprocess.run(["../../bin/ffmpeg", "-framerate", "120", "-i", input_file, "-crf", "3", "-c:v", "libvpx-vp9", output_file])
-    
-    # compress ratio evaluation
-    size = 0
-    folderpath = os.path.abspath(os.path.dirname(input_path))
-    for path, dirs, files in os.walk(folderpath):
-        for f in files:
-            fp = os.path.join(path, f)
-            size += os.path.getsize(fp)
-            
+
     endTime = time.time()
-            
-    videosize = os.stat(os.path.abspath(output_path)).st_size   
-    print(f"encoded in {endTime - startTime} seconds.")  # print the time taken to encode the video
-    print("compression ratio: " + str(size/videosize))    
+    print(f"Encoded in {endTime - startTime} seconds.")  # print the time taken to encode the video
+    
+    
+def compressor_HEVC(input_path, output_path):
+    # set input and output paths
+    input_file = input_path
+    output_file = output_path
+    
+    startTime = time.time()
+    
+    # call ffmpeg to compress the video
+    subprocess.run(["../../bin/ffmpeg", "-framerate", "120", "-i", input_file, "-crf", "3", "-c:v", "libvpx-vp9", output_file])
+
+    endTime = time.time()
+    print(f"Encoded in {endTime - startTime} seconds.")  # print the time taken to encode the video
+    
+    
+def compressor_VP9(input_path, output_path):
+    # set input and output paths
+    input_file = input_path
+    output_file = output_path
+    
+    startTime = time.time()
+    
+    # call ffmpeg to compress the video
+    subprocess.run(["./ffmpeg/bin/ffmpeg", "-framerate", "120", "-i", input_file, "-c:v", "libvpx-vp9", "-lossless", "1", output_file])
+
+    endTime = time.time()
+    print(f"Encoded in {endTime - startTime} seconds.")  # print the time taken to encode the video
+    
+    
+def compressor_AV1(input_path, output_path):
+    # set input and output paths
+    input_file = input_path
+    output_file = output_path
+    
+    startTime = time.time()
+    
+    # call ffmpeg to compress the video
+    subprocess.run(["./ffmpeg/bin/ffmpeg", "-framerate", "120", "-i", input_file, "-c:v", "libaom-av1", "-aom-params", "lossless=1", output_file])
+
+    endTime = time.time()
+    print(f"Encoded in {endTime - startTime} seconds.")  # print the time taken to encode the video
+    
+
+def compressor_HUFFYUV(input_path, output_path):
+    # set input and output paths
+    input_file = input_path
+    output_file = output_path
+    
+    startTime = time.time()
+    
+    # call ffmpeg to compress the video
+    subprocess.run(["./ffmpeg/bin/ffmpeg", "-framerate", "120", "-i", input_file, "-c:v", "huffyuv", output_file])
+
+    endTime = time.time()
+    print(f"Encoded in {endTime - startTime} seconds.")  # print the time taken to encode the video
+    
+    
+def compressor_UTVIDEO(input_path, output_path):
+    # set input and output paths
+    input_file = input_path
+    output_file = output_path
+    
+    startTime = time.time()
+    
+    # call ffmpeg to compress the video
+    subprocess.run(["./ffmpeg/bin/ffmpeg", "-framerate", "120", "-i", input_file, "-c:v", "utvideo", output_file])
+
+    endTime = time.time()
+    print(f"Encoded in {endTime - startTime} seconds.")  # print the time taken to encode the video
+    
+    
+def compressor_FFV1(input_path, output_path):
+    # set input and output paths
+    input_file = input_path
+    output_file = output_path
+    
+    startTime = time.time()
+    
+    # call ffmpeg to compress the video
+    subprocess.run(["./ffmpeg/bin/ffmpeg", "-framerate", "120", "-i", input_file, "-level", "3", "-coder", "2", "-c:v", "ffv1", output_file])
+
+    endTime = time.time()
+    print(f"Encoded in {endTime - startTime} seconds.")  # print the time taken to encode the video
--- src/video_compression.py
+++ src/video_compression.py
@@ -10,11 +10,40 @@ if(len(sys.argv[1:]) == 3):
     input_path = sys.argv[2]
     output_path = sys.argv[3]
 else:
-    print("Specify those parameters:\n\t-Compression algorithm (HEVC-VS, AV1-VS, VP9-VS)\n\t-Input path (es. ./ArtGallery2/Frame_%3d.png)\n\t-Output path (es. output.mp4)")
+    print("Specify those parameters:\n\t-Compression algorithm (HEVC, AV1, VP9, HUFFYUV, UTVIDEO, FFV1, HEVC-VS, AV1-VS, VP9-VS)\n\t-Input path (es. ./ArtGallery2/Frame_%3d.png)\n\t-Output path (es. output.mp4)")
     exit()
     
-start_time = time.time()
 match algorithm:
+    case "HEVC":
+        if(pathlib.Path(output_path).suffix == ".mp4"):
+            compressors.compressor_HEVC(input_path, output_path)
+        else:
+           print("HEVC needs an output file with extension .mp4")
+    case "AV1":
+        if(pathlib.Path(output_path).suffix == ".mkv"):
+            compressors.compressor_AV1(input_path, output_path)
+        else:
+            print("AV1 needs an output file with extension .mkv")
+    case "VP9":
+        if(pathlib.Path(output_path).suffix == ".webm"):
+            compressors.compressor_VP9(input_path, output_path)
+        else:
+            print("VP9 needs an output file with extension .webm")
+    case "HUFFYUV":
+        if(pathlib.Path(output_path).suffix == ".avi"):
+            compressors.compressor_HUFFYUV(input_path, output_path)
+        else:
+           print("HUFFYUV needs an output file with extension .avi")
+    case "UTVIDEO":
+        if(pathlib.Path(output_path).suffix == ".avi"):
+            compressors.compressor_UTVIDEO(input_path, output_path)
+        else:
+           print("UTVIDEO needs an output file with extension .avi")
+    case "FFV1":
+        if(pathlib.Path(output_path).suffix == ".avi"):
+            compressors.compressor_FFV1(input_path, output_path)
+        else:
+           print("FFV1 needs an output file with extension .avi")
     case "HEVC-VS":
         if(pathlib.Path(output_path).suffix == ".mp4"):
             compressors.compressor_HEVCVS(input_path, output_path)
